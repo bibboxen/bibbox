aaf66b180be74f1e1958f973fe03aacac67e1626
Troels Ugilt Jensen <tujensen@gmail.com>
2016-09-13 15:16:35 +0200

    Issue #BIB-5 by tuj: Added translations.

--- public/js/app.js
+++ public/js/app.js
@@ -4,4 +4,36 @@
 */
 
 // Define the angular application.
-angular.module('BibBox', [ 'ngRoute' ]);
+angular.module('BibBox', [ 'ngRoute', 'pascalprecht.translate' ]);
+
+var enTranslations = {
+  "menu.borrow": "Borrow",
+  "menu.status": "Status/Renew",
+  "menu.reservations": "Reservations",
+  "menu.return": "Return",
+
+  "index.heading": "Choose desired function",
+
+  "language.en": "English",
+  "language.da": "Danish"
+};
+
+var daTranslations = {
+  "menu.borrow": "Udlån",
+  "menu.status": "Status/Forny",
+  "menu.reservations": "Reservationer",
+  "menu.return": "Aflevering",
+
+  "index.heading": "Vælg ønsket funktion",
+
+  "language.en": "Engelsk",
+  "language.da": "Dansk"
+};
+
+
+angular.module('BibBox').config(['$translateProvider', function ($translateProvider) {
+  $translateProvider
+    .translations('en', enTranslations)
+    .translations('da', daTranslations)
+    .preferredLanguage('en');
+}]);
--- public/js/controllers/mainController.js
+++ public/js/controllers/mainController.js
@@ -1,31 +1,48 @@
 /**
  * Index page.
  */
-angular.module('BibBox').controller('IndexController', ['$scope', '$http', '$window', '$location',
-  function($scope, $http, $window, $location) {
+angular.module('BibBox').controller('IndexController', ['$scope', '$http', '$window', '$location', '$translate',
+  function($scope, $http, $window, $location, $translate) {
     "use strict";
 
     $scope.buttons = [
       {
-        "text": "Udlån",
+        "text": "menu.borrow",
         "url": "/#/borrow",
         "icon": "glyphicon-tasks"
       },
       {
-        "text": "Status/Forny",
+        "text": "menu.status",
         "url": "/#/status",
         "icon": "glyphicon-refresh"
       },
       {
-        "text": "Reservationer",
+        "text": "menu.reservations",
         "url": "/#/reservations",
         "icon": "glyphicon-list-alt"
       },
       {
-        "text": "Aflevering",
+        "text": "menu.return",
         "url": "/#/return",
         "icon": "glyphicon-time"
       }
     ];
+
+    $scope.languages = [
+      {
+        "text": "language.da",
+        "langKey": "da",
+        "icon": "img/flags/DK.png"
+      },
+      {
+        "text": "language.en",
+        "langKey": "en",
+        "icon": "img/flags/GB.png"
+      }
+    ];
+
+    $scope.changeLanguage = function changeLanguage(langKey) {
+      $translate.use(langKey);
+    }
   }
 ]);
