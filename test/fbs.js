e44ab700a6e6c3089537c23716c4e7c911397e6c
Jesper Kristensen <cableman@linuxdev.dk>
2016-09-22 13:54:28 +0200

    Issue #BIB-25: Added date parse tests

--- test/fbs.js
+++ test/fbs.js
@@ -113,6 +113,21 @@ it('XML parser (complex message)', function(done) {
   done();
 });
 
+it('Date parser', function(done) {
+  // Message "23" - patron status (needs xml to parse).
+  var xml = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?><ns2:sip xmlns:ns2="http://axiell.com/Schema/sip.xsd"><response>64              00920160921    094230000000000002000100000004AODK-761500|AALN:3208100032|AETestlåner Aarhus|BZ9999|CA9999|CB9999|BLY|CQY|BHDKK|BV175.00|CC200.00|AS|AT|AU5010941603%20161021%Harry Potter &amp; the chamber of secrets%Rowling Joanne K.%a%xx%83|AU4933448504%20161021%Fankultur og fanfiktion%Petersen Anne%a%xx%30.13|AV%289097%20160920%175.00%%%%%|BU|CD51128567%%20170320%I potter &amp; krukker%Dalby Claus%a%xx%63.5|CD51011341%%20170320%Krydderurter i have og køkken%Olesen Anemette%a%xx%63.54|CD45626482%%20170320%Harry Potter e la pietra filosofale%Rowling Joanne K.%a%xx%Uden klassemærke|CD50926613%%20170320%Mad fra små haver%Segall Barbara%a%xx%63.54|BDJesper Kristensen%Hack Kampmannsplads 2%8000%Aarhus%DK|BEjeskr@aarhus.dk|</response></ns2:sip>';
+  var res = new Response(xml, 'AO');
+
+  var date = '20160920    233001';
+  res.parseDate(date).should.equal(1474407001000);
+
+  date = '20160920';
+  res.parseDate(date).should.equal(1474322400000);
+
+
+  done();
+});
+
 it('Login (correct)', function(done) {
 	setup().then(function (app) {
 		app.services.fbs.login('1234567890', '1234').then(function (val) {
