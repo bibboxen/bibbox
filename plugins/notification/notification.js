ae3384de6506d42655ec6c7f7d0ce280ef4099ac
Jesper Kristensen <cableman@linuxdev.dk>
2016-10-05 16:39:27 +0200

    Issue #BIB-59: Started in markupjs tempaltes

--- plugins/notification/notification.js
+++ plugins/notification/notification.js
@@ -5,34 +5,43 @@
 
 var wkhtmltopdf = require('wkhtmltopdf');
 var printer = require('printer');
-var handlebars = require('handlebars');
-var i18n = require("i18n");
+var Mark = require('markup-js');
 
 var fs = require('fs');
 var Q = require('q');
 
 
-var Notification = function Printer() {
+var Notification = function Notification(bus) {
   "use strict";
+
+  this.bus = bus;
+
+  this.mailTemplate = fs.readFileSync(__dirname + '/templates/receiptMail.html', 'utf8');
+
+  /**
+   * New data MarkupJS pipe format.
+   *
+   * @param milliseconds
+   *   The timestamp to format.
+   *
+   * @returns {string}
+   *   The data formatted.
+   */
+  Mark.pipes.date = function (milliseconds) {
+    var date = new Date(milliseconds);
+
+    // Prefix month with '0';
+    var month = ('0' + (date.getMonth() + 1)).slice(-2);
+
+    // Only get latest 2 char of yera.
+    var year = ('' + date.getFullYear()).slice(-2);
+
+    return '' + date.getDate() + '/' + month + '/' + year;
+  };
 
   // Get default printer name and use that as printer.
   this.defaultPrinterName = printer.getDefaultPrinterName();
-
-  i18n.configure({
-    locales:['en', 'da'],
-    directory: __dirname + '/locales'
-  });
-
-  // Add translation helper (https://www.npmjs.com/package/i18n).
-  // <div>{{translate myVar}}</div>
-  Handlebars.registerHelper('translate',
-     function(str){
-       return (i18n != undefined ? i18n.__(str) : str);
-     }
-  );
 };
-
-
 
 /**
  * Get name of the default system printer.
@@ -87,7 +96,22 @@ Notification.prototype.reservationsReceipt = function reservationsReceipt(userna
  * @param mail
  */
 Notification.prototype.statusReceipt = function statusReceipt(username, password, mail) {
+  var self = this;
 
+  this.bus.once('notification.statusReceipt', function(data) {
+    console.log(data);
+    console.log('--------------------------------');
+    var result = Mark.up(self.mailTemplate, data);
+    console.log('--------------------------------');
+    console.log(result);
+
+  });
+
+  this.bus.emit('fbs.patron', {
+    'username': username,
+    'password': password,
+    'busEvent': 'notification.statusReceipt'
+  });
 };
 
 Notification.prototype.test = function test(file) {
@@ -111,7 +135,9 @@ Notification.prototype.test = function test(file) {
  */
 module.exports = function (options, imports, register) {
 
-	var notification = new Notification();
+	var notification = new Notification(imports.bus);
+
+  //notification.statusReceipt('3208100032', '12345', false);
 
   register(null, {
     "notification": notification
