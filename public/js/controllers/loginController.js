a112ce0284c87b007c0eae47e2a28108b0956c21
Jesper Kristensen <cableman@linuxdev.dk>
2016-11-21 14:25:51 +0100

    Issue #BIB-160: Fixed barcode callbacks

--- public/js/controllers/loginController.js
+++ public/js/controllers/loginController.js
@@ -138,12 +138,12 @@ angular.module('BibBox').controller('LoginController', ['$scope', '$controller',
      * @param data
      *   The barcode scanned.
      */
-    $scope.$on('barcodeScanned', function barcodeScanned(event, data) {
+    $scope.barcodeScanned = function barcodeScanned(event, data) {
       if ($scope.display === 'start') {
         $scope.user.username = data;
         $scope.usernameEntered();
       }
-    });
+    };
 
     /**
      * Barcode error handler.
@@ -153,10 +153,10 @@ angular.module('BibBox').controller('LoginController', ['$scope', '$controller',
      * @param err
      *   The error thrown.
      */
-    $scope.$on('barcodeError', function barcodeError(event, err) {
+    $scope.barcodeError = function barcodeError(event, err) {
       // @TODO: inform user that barcode as failed and switch to manual.
       console.log('barcodeError', event, err);
-    });
+    };
 
     // Start listen to barcode events.
     barcodeService.start($scope);
