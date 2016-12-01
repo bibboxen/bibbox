f627e57265f765be2eed968406769ab5fed530a7
Jesper Kristensen <cableman@linuxdev.dk>
2016-12-01 17:03:39 +0100

    Issue #BIB-191: Added new network connection tester

---
+++ plugins/network/network_tester.js
@@ -0,0 +1,55 @@
+/**
+ * Helper program to connect to remote host to check network connection.
+ */
+
+'use strict';
+
+var net = require('net');
+
+var socket = new net.Socket();
+var host = process.argv[2];
+var port = parseInt(process.argv[3], 10);
+
+/**
+ * Connect to remote host.
+ */
+var connectTimeout = parseInt(process.argv[4], 10);
+socket.setTimeout(connectTimeout);
+socket.connect(port, host);
+
+/**
+ * Connected to remote host.
+ */
+socket.on('connect', function () {
+  socket.destroy();
+  process.send({
+    error: false,
+  });
+});
+
+/**
+ * Connection error.
+ */
+socket.on('error', function (err) {
+  var msg = err && err.message || 'connect ECONNREFUSED';
+  socket.destroy();
+  process.send({
+    error: true,
+    message: msg
+  });
+});
+
+/**
+ * Connection timed out.
+ */
+socket.on('timeout', function (err) {
+  socket.destroy();
+  process.send({
+    error: true,
+    message: 'Timed-out: ' + connectTimeout
+  });
+
+  // As the process will hang until the OS times out the socket... this program
+  // will not exit before that. So we force that exit with a ctrl+c.
+  process.kill(process.pid, 'SIGINT');
+});
