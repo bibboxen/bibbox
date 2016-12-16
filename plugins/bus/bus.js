66b5f24ce198e78aa95a0fba64382f558e9201d3
Jesper Kristensen <cableman@linuxdev.dk>
2016-12-16 17:35:57 +0100

    Issue #BIB-176: Added event wrappers to handle memory leak

--- plugins/bus/bus.js
+++ plugins/bus/bus.js
@@ -26,17 +26,102 @@ module.exports = function (options, imports, register) {
     maxListeners: 20
   });
 
+  var events = {};
+
+  /**
+   * Remove events from book keeping.
+   *
+   * @param {string} type
+   *   Event type to remove.
+   */
+  function removeEvent(type) {
+    if (events.hasOwnProperty(type)) {
+      var eventName = events[type];
+
+      // Clean up book keeping books.
+      delete events[type];
+      delete events[eventName];
+
+      // Remove the unused listener.
+      emitter.removeAllListeners(eventName);
+    }
+  }
+
+  /**
+   * Emit event wrapper to do booking.
+   *
+   * This exist to remove unused listeners to prevent memory leaks.
+   *
+   * @param {string} type
+   *   The event type/name.
+   * @param {*} data
+   *   The data emitted.
+   */
+  emitter.emitBibboxWrapper = function(type, data) {
+    if (Object.prototype.toString.call(data) === '[object Object]') {
+      if (data.hasOwnProperty('busEvent') && data.hasOwnProperty('errorEvent')) {
+        // We use the same pattern to send events into the bus. So we do some
+        // book keeping to be able to remove the unused event handler and free
+        // memory.
+        events[data.busEvent] = data.errorEvent;
+        events[data.errorEvent] = data.busEvent;
+      }
+    }
+
+    // Send the event on the the normal handler.
+    emitter.emit.apply(this, arguments);
+  };
+
+  /**
+   * Once wrapper to remove unused listeners.
+   *
+   * @param {string} type
+   *   The event type/name.
+   */
+  emitter.onceBibboxWrapper = function(type) {
+    removeEvent(type);
+
+    // Send the event on the the normal handler.
+    emitter.once.apply(this, arguments);
+  };
+
+  /**
+   * RemoveListener wrapper to remove unused listeners.
+   *
+   * @param {string} type
+   *   The event type/name.
+   */
+  emitter.removeListenerBibboxWrapper = function(type) {
+    removeEvent(type);
+
+    // Send the event on the the normal handler.
+    emitter.removeListener.apply(this, arguments);
+  };
+
+  /**
+   * RemoveAllListeners wrapper to remove unused listeners.
+   *
+   * @param {string} type
+   *   The event type/name.
+   */
+  emitter.removeAllListenersBibboxWrapper = function(type) {
+    removeEvent(type);
+
+    // Send the event on the the normal handler.
+    emitter.removeAllListeners.apply(this, arguments);
+  };
+
   register(null, {
     bus: {
-      emit: emitter.emit,
+      emit: emitter.emitBibboxWrapper,
       onAny: emitter.onAny,
       offAny: emitter.offAny,
       on: emitter.on,
       off: emitter.off,
-      once: emitter.once,
+      once: emitter.onceBibboxWrapper,
       many: emitter.many,
-      removeListener: emitter.removeListener,
-      removeAllListeners: emitter.removeAllListeners
+      removeListener: emitter.removeListenerBibboxWrapper,
+      removeAllListeners: emitter.removeAllListenersBibboxWrapper
     }
   });
 };
