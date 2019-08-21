6cf709354c293cd6a34880ffb2ea9ed492875226
Jesper Kristensen <cableman@linuxdev.dk>
2019-08-21 13:45:20 +0200

    BIB-357: Added support for docker setup

--- install.sh
+++ install.sh
@@ -6,6 +6,10 @@ npm install --${1:-production}
 # Install plugin dependencies.
 for folder in plugins/*; do
   if [ -d $folder ]; then
+    echo
+    echo '-----------------------'
+    echo "==> ${folder}"
+    echo '-----------------------'
     cd $folder; npm install --${1:-production}; cd ../..;
   fi
 done
--- upgrade.sh
+++ upgrade.sh
@@ -7,6 +7,10 @@ npm install --${1:-production}
 # Install plugin dependencies.
 for folder in plugins/*; do
   if [ -d $folder ]; then
+    echo
+    echo '-----------------------'
+    echo "==> ${folder}"
+    echo '-----------------------'
     cd $folder
     echo ${folder}
     rm -rf node_modules
