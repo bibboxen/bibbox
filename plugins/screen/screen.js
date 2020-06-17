71fca9b9fb071252ffcc7c65b8670f1e09eda502
Jesper Kristensen <cableman@linuxdev.dk>
2020-06-17 14:43:33 +0200

    BIB-366: Tried to get screen to load config - II

--- plugins/screen/screen.js
+++ plugins/screen/screen.js
@@ -28,7 +28,10 @@ var Screen = function Network(bus) {
   console.error('LOADED SCREEN');
 
   bus.once(busEvent, function (config) {
-    this.setup(config);
+    if (config.hasOwnProperty('screen')) {
+      console.log(config.screen);
+      this.setup(config.screen);
+    }
   });
 
   bus.once(errorEvent, function (config) {
@@ -43,15 +46,16 @@ var Screen = function Network(bus) {
 
 Screen.prototype.setup = function setup(config) {
 
+  console.log('HERHE')
   console.log(config);
 
-  this.sceenOnjob = new ScreenCronJob('* * * * * *', function() {
-    console.log('You will see this message every second');
-  }, null, true, 'America/Los_Angeles');
-
-  this.sceenOffjob = new ScreenCronJob('* * * * * *', function() {
-    console.log('You will see this message every second');
-  }, null, true, 'America/Los_Angeles');
+  // this.sceenOnjob = new ScreenCronJob('* * * * * *', function() {
+  //   console.log('You will see this message every second');
+  // }, null, true, 'America/Los_Angeles');
+  //
+  // this.sceenOffjob = new ScreenCronJob('* * * * * *', function() {
+  //   console.log('You will see this message every second');
+  // }, null, true, 'America/Los_Angeles');
 };
 
 
@@ -77,7 +81,9 @@ module.exports = function (options, imports, register) {
     var errorEvent = 'screen.config.error' + uniqid();
 
     bus.once(busEvent, function (config) {
-      screen.setup(config);
+      if (config.hasOwnProperty('screen')) {
+        screen.setup(config.screen);
+      }
     });
 
     bus.emit('ctrl.config.config', {
