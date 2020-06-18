a66ed413ee9c54f531cfaf28d46c137e8e4a7787
Jesper Kristensen <cableman@linuxdev.dk>
2020-06-18 11:29:40 +0200

    BIB-366: Added cron jobs to turn screen on/off

--- plugins/screen/screen.js
+++ plugins/screen/screen.js
@@ -5,10 +5,10 @@
 
 'use strict';
 
-var ScreenCronJob = require('cron').CronJob;
-var fork = require('child_process').fork;
-var debug = require('debug')('bibbox:screen');
-var uniqid = require('uniqid');
+const ScreenCronJob = require('cron').CronJob;
+const debug = require('debug')('bibbox:screen');
+const uniqid = require('uniqid');
+const exec = require('child_process').exec;
 
 /**
  * The Screen object.
@@ -20,22 +20,18 @@ var uniqid = require('uniqid');
  */
 var Screen = function Network(bus) {
   this.bus = bus;
+  this.sceenOnjob = null;
+  this.sceenOffjob = null;
 
   // Load configuration.
   var busEvent = 'screen.config.loaded' + uniqid();
   var errorEvent = 'screen.config.error' + uniqid();
 
-  console.error('LOADED SCREEN');
-
+  var self = this;
   bus.once(busEvent, function (config) {
     if (config.hasOwnProperty('screen')) {
-      console.log(config.screen);
-      this.setup(config.screen);
+      self.setup(config.screen);
     }
-  });
-
-  bus.once(errorEvent, function (config) {
-    console.error('No config');
   });
 
   bus.emit('ctrl.config.config', {
@@ -45,19 +41,67 @@ var Screen = function Network(bus) {
 };
 
 Screen.prototype.setup = function setup(config) {
+  var self = this;
 
-  console.log('HERHE')
-  console.log(config);
+  // Build time in cron format from configuration format.
+  var on = config.on.split(':').reverse().join(' ');
+  var off = config.off.split(':').reverse().join(' ');
 
-  // this.sceenOnjob = new ScreenCronJob('* * * * * *', function() {
-  //   console.log('You will see this message every second');
-  // }, null, true, 'America/Los_Angeles');
-  //
-  // this.sceenOffjob = new ScreenCronJob('* * * * * *', function() {
-  //   console.log('You will see this message every second');
-  // }, null, true, 'America/Los_Angeles');
+  // Stop jobs if they are running.
+  this.stop();
+
+  // Set new jobs.
+  this.sceenOnjob = new ScreenCronJob(on + ' * * *', function() {
+    self.exec('on');
+  }, null, true, 'Europe/Copenhagen');
+
+  this.sceenOffjob = new ScreenCronJob(off + ' * * *', function() {
+    self.exec('off');
+  }, null, true, 'Europe/Copenhagen');
+
+  // Start the jobs.
+  this.start();
 };
 
+/**
+ * Run screen on/off command.
+ *
+ * @param state
+ *   This is string "on" or "off".
+ */
+Screen.prototype.exec = function exec(state) {
+  exec('xset -display :0 dpms force ' + state, function (err, stdout, stderr) {
+    if (err) {
+      debug('Screen error: ' + err.message);
+      return;
+    }
+    debug('Screen error: ' +  stderr);
+  });
+}
+
+/**
+ * Start the jobs if they are defined.
+ */
+Screen.prototype.start = function start() {
+  if (this.sceenOnjob != null) {
+    this.sceenOnjob.start();
+  }
+  if (this.sceenOffjob != null) {
+    this.sceenOffjob.start();
+  }
+}
+
+/**
+ * Stop the jobs if they are defined.
+ */
+Screen.prototype.stop = function stop() {
+  if (this.sceenOnjob != null) {
+    this.sceenOnjob.stop();
+  }
+  if (this.sceenOffjob != null) {
+    this.sceenOffjob.stop();
+  }
+}
 
 /**
  * Register the plugin with architect.
